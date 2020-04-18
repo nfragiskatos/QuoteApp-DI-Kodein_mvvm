115c3b43dce44471719856348609871a56c5612e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 13:44:07 -0700

    - Added functions to add a quote and get all quotes to/from the repository

--- app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/ui/quotes/QuotesViewModel.kt
+++ app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/ui/quotes/QuotesViewModel.kt
@@ -1,10 +1,14 @@
 package com.nfragiskatos.quoteapp_di_kodein_mvvm.ui.quotes
 
 import androidx.lifecycle.ViewModel
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.model.Quote
 import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.repository.QuoteRepository
 
 /**
  * Created by Nicholas Fragiskatos on 4/18/2020.
  */
 class QuotesViewModel(private val quoteRepository: QuoteRepository) : ViewModel() {
+
+    fun addQuote(quote: Quote) = quoteRepository.addQuote(quote)
+    fun getQuotes() = quoteRepository.getQuotes()
 }
