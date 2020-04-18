a5ddba1e56bdc9d706b18c2352f3e4db0575158b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 12:57:03 -0700

    - add mutable live data variable for observers - init the mutable live data with our "db" member variable - implemented addQuote and getQuote functions

--- app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/data/db/QuoteDaoFakeImpl.kt
+++ app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/data/db/QuoteDaoFakeImpl.kt
@@ -1,6 +1,7 @@
 package com.nfragiskatos.quoteapp_di_kodein_mvvm.data.db
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.model.Quote
 
 /**
@@ -8,14 +9,22 @@ import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.model.Quote
  */
 class QuoteDaoFakeImpl : QuoteDao {
 
+    // Fake DB table
     private val quoteList = mutableListOf<Quote>()
 
-    override fun addQuote(quote: Quote) {
-        TODO("Not yet implemented")
+    private val quotes = MutableLiveData<List<Quote>>()
+
+    init {
+        quotes.value = quoteList
     }
 
-    override fun getQuotes(): LiveData<List<Quote>> {
-        TODO("Not yet implemented")
+
+    override fun addQuote(quote: Quote) {
+        quoteList.add(quote)
+        // Need to reset this so quotes knows to notify its observers.
+        quotes.value = quoteList
     }
+
+    override fun getQuotes(): LiveData<List<Quote>> = quotes as LiveData<List<Quote>>
 
 }
