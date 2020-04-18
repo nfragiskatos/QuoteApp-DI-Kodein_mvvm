21b780d6e6728ba388b4eed9b96fc8a2123554c0
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 15:33:14 -0700

    - Added all binding for injection in our application 1. Database interface is bound to the concrete implementation of DatabaseFakeImp 2. QuoteDao interface is bound to the concrete implementation of QuoteDaoFakeImpl that we get from the instance of our previously bound Database 3. QuoteRepository interface is bound to the concrete implementation QuoteRepositoryImpl and we are passing in our instance of QuoteDao as the parameter 4. QuotesViewModelFactory is bound, but not through an interface and not singleton, so instead we used a provider to create the binding and we are passing in our instance of the previously bound QuoteRepository

--- app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/QuotesApplication.kt
+++ app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/QuotesApplication.kt
@@ -1,7 +1,27 @@
 package com.nfragiskatos.quoteapp_di_kodein_mvvm
 
+import android.app.Application
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.db.Database
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.db.DatabaseFakeImpl
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.db.QuoteDao
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.repository.QuoteRepository
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.repository.QuoteRepositoryImpl
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.ui.quotes.QuotesViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
 /**
  * Created by Nicholas Fragiskatos on 4/18/2020.
  */
-class QuotesApplication {
+class QuotesApplication : Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
+        bind<Database>() with singleton { DatabaseFakeImpl() }
+        bind<QuoteDao>() with singleton { instance<Database>().quoteDao }
+        bind<QuoteRepository>() with singleton { QuoteRepositoryImpl(instance<QuoteDao>()) }
+        bind() from provider { QuotesViewModelFactory(instance()) }
+    }
 }
