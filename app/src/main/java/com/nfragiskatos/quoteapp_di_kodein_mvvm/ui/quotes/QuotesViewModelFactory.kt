fbaf12db0941e163b25b2e83acde28b61a6a4694
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 13:36:14 -0700

    - Factory to create our Quotes View Model for us. Will accept a constructor parameter of type QuoteRepository (interface) that will be injected. - ViewModels need a handle to the repository, so the Factory needs the repository handle also to pass it to the ViewModel when it's created.

---
+++ app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/ui/quotes/QuotesViewModelFactory.kt
@@ -0,0 +1,9 @@
+package com.nfragiskatos.quoteapp_di_kodein_mvvm.ui.quotes
+
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.repository.QuoteRepository
+
+/**
+ * Created by Nicholas Fragiskatos on 4/18/2020.
+ */
+class QuotesViewModelFactory(private val quotesRepository: QuoteRepository) {
+}
