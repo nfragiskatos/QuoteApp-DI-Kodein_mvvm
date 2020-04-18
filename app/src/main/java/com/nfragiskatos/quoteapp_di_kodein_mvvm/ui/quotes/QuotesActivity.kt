31524563d0aeab365cd040cefcccdb853c5ea9e0
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 12:35:26 -0700

    - Created basic package structure for data and ui - Changed MainActivity -> QuotesActivity and moved it to ui package - Changed activity_main -> activity_quotes

---
+++ app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,13 @@
+package com.nfragiskatos.quoteapp_di_kodein_mvvm.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nfragiskatos.quoteapp_di_kodein_mvvm.R
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
