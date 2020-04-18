884fc8efd23eaab2211065542a09ecd1838cbbb2
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 12:24:07 -0700

    Initial commit

---
+++ app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nfragiskatos.quoteapp_di_kodein_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
