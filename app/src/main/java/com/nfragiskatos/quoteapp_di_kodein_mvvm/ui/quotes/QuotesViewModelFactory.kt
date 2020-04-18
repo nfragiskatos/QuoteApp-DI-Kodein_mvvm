30dccb86e548fc831e4977cee45cd84b4e18fb91
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-18 13:41:00 -0700

    - Had class extend the ViewModelProvider.NewInstanceFactory class - Overrode the create function

--- app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/ui/quotes/QuotesViewModelFactory.kt
+++ app/src/main/java/com/nfragiskatos/quoteapp_di_kodein_mvvm/ui/quotes/QuotesViewModelFactory.kt
@@ -1,9 +1,15 @@
 package com.nfragiskatos.quoteapp_di_kodein_mvvm.ui.quotes
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.nfragiskatos.quoteapp_di_kodein_mvvm.data.repository.QuoteRepository
 
 /**
  * Created by Nicholas Fragiskatos on 4/18/2020.
  */
-class QuotesViewModelFactory(private val quotesRepository: QuoteRepository) {
+class QuotesViewModelFactory(private val quotesRepository: QuoteRepository) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return QuotesViewModel(quotesRepository) as T
+    }
 }
